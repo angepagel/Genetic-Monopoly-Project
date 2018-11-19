d9cab5b96c775b85fa29167187f4476b8341b0fe
Inkan <inkandev@gmail.com>
2018-11-19 14:20:38 +0100

    Modification de la boîte de dialogue GererMesProprietes
    Édition du fichier .fxml et du contrôleur.

--- src/monopoly/controleur/ControleurGererMesProprietes.java
+++ src/monopoly/controleur/ControleurGererMesProprietes.java
@@ -1,5 +1,50 @@
 package monopoly.controleur;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import monopoly.vue.dialogue.DialogueVoirDetailsPropriete;
+
+import java.io.IOException;
+
 public class ControleurGererMesProprietes {
+
+    // TODO : Remplir le tableau
+
+    @FXML
+    private TableView tableauVoirMesProprietes;
+
+    @FXML
+    private TableColumn colNom;
+
+    @FXML
+    private TableColumn colAmelioration;
+
+    @FXML
+    private TableColumn colLoyer;
+
+    @FXML
+    private TableColumn colHypotheque;
+
+    @FXML
+    public void actionDetailsPropriete(ActionEvent event) throws IOException {
+        new DialogueVoirDetailsPropriete();
+    }
+
+    @FXML
+    public void actionConstruire(ActionEvent event) {
+        // TODO : Gérer l'amélioration de la propriété sélectionnée
+    }
+
+    @FXML
+    public void actionVendre(ActionEvent event) {
+        // TODO : Gérer la vente de la propriété sélectionnée
+    }
+
+    @FXML
+    public void actionHypothequer(ActionEvent event) {
+        // TODO : Gérer l'hypothèque de la propriété sélectionnée
+    }
 
 }
