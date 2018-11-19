196360b59d9a2c9c453de5a4f0a8db520baeb2db
Sekanor <sekanor21@gmail.com>
2018-11-19 01:27:20 +0100

    Ajout d'attributs pour les dés

--- src/monopoly/controleur/MainControleur.java
+++ src/monopoly/controleur/MainControleur.java
@@ -2,9 +2,17 @@ package monopoly.controleur;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import monopoly.modele.Des;
 
 public class MainControleur {
+
+    @FXML
+    private ImageView de_1;
+
+    @FXML
+    private ImageView de_2;
 
     @FXML
     public void actionVoirMesProprietes(ActionEvent event) {
@@ -23,6 +31,9 @@ public class MainControleur {
         int sommes = LesDes.lancer();
         int de1 = LesDes.valDe1();
         int de2 = LesDes.valDe2();
+
+        Image spr_de1 = new Image("file:src/monopoly/vue/sprites/dice_1.png", true);
+        this.de_1.setImage(spr_de1);
     }
 
     @FXML
