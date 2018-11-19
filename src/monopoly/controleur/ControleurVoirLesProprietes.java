05c4c2227024a8080d1af6e468bad8998d696f92
Inkan <inkandev@gmail.com>
2018-11-19 14:41:37 +0100

    Modification de la boîte de dialogue VoirLesProprietes
    Édition du fichier .fxml et du contrôleur.

--- src/monopoly/controleur/ControleurVoirLesProprietes.java
+++ src/monopoly/controleur/ControleurVoirLesProprietes.java
@@ -1,5 +1,35 @@
 package monopoly.controleur;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import monopoly.vue.dialogue.DialogueVoirDetailsPropriete;
+
+import java.io.IOException;
+
 public class ControleurVoirLesProprietes {
+
+    // TODO : Remplir le tableau
+
+    @FXML
+    private TableView tableauVoirLesProprietes;
+
+    @FXML
+    private TableColumn colNom;
+
+    @FXML
+    private TableColumn colProprietaire;
+
+    @FXML
+    private TableColumn colAmelioration;
+
+    @FXML
+    private TableColumn colLoyer;
+
+    @FXML
+    public void actionDetailsPropriete(ActionEvent event) throws IOException {
+        new DialogueVoirDetailsPropriete();
+    }
 
 }
