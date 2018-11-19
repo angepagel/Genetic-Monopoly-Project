d1aeb44a74cf9b889a6d17d5b2fd5fb4555a8ac4
Inkan <inkandev@gmail.com>
2018-11-19 15:58:28 +0100

    Modification de la boîte de dialogue VoirDetailsProprietes
    Édition du fichier .fxml et du contrôleur.

--- src/monopoly/controleur/ControleurVoirDetailsPropriete.java
+++ src/monopoly/controleur/ControleurVoirDetailsPropriete.java
@@ -1,5 +1,35 @@
 package monopoly.controleur;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class ControleurVoirDetailsPropriete {
+
+    @FXML
+    private Label labelNom;
+
+    @FXML
+    private Label labelProprietaire;
+
+    @FXML
+    private Label labelLoyer;
+
+    @FXML
+    private Label labelLoyerAmelioration1;
+
+    @FXML
+    private Label labelLoyerAmelioration2;
+
+    @FXML
+    private Label labelLoyerAmelioration3;
+
+    @FXML
+    private Label labelLoyerAmelioration4;
+
+    @FXML
+    private Label labelLoyerAmelioration5;
+
+    @FXML
+    private Label labelHypotheque;
 
 }
