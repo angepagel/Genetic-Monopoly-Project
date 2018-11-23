6de724ac37abe58aa9d204f53f71f418649b7420
Inkan <inkandev@gmail.com>
2018-11-23 14:17:31 +0100

    Modification de la boîte de dialogue Echanger et de son contrôleur

--- src/monopoly/controleur/ControleurEchanger.java
+++ src/monopoly/controleur/ControleurEchanger.java
@@ -1,5 +1,33 @@
 package monopoly.controleur;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import monopoly.vue.dialogue.DialogueVoirDetailsPropriete;
+
+import java.io.IOException;
+
 public class ControleurEchanger {
+
+    @FXML
+    private void actionDetailsPropriete1(ActionEvent event) throws IOException {
+        new DialogueVoirDetailsPropriete();
+        // TODO : Passer une propriété en paramètre
+    }
+
+    @FXML
+    private void actionDetailsPropriete2(ActionEvent event) throws IOException {
+        new DialogueVoirDetailsPropriete();
+        // TODO : Passer une propriété en paramètre
+    }
+
+    @FXML
+    private void actionProposer(ActionEvent event) {
+        // TODO : Gérer une proposition d'argent pour l'échange
+    }
+
+    @FXML
+    private void actionDemander(ActionEvent event) {
+        // TODO : Gérer une demande d'argent pour l'échange
+    }
 
 }
