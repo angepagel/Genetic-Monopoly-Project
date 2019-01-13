8b8acf8ded31ff33fec24c967c4c5f14073c063f
Ange Pagel <inkandev@gmail.com>
2019-01-13 20:22:53 +0100

    Développement du DialogueVoirMesProprietes

--- src/monopoly/vue/controleurFXML/ControleurVoirMesProprietes.java
+++ src/monopoly/vue/controleurFXML/ControleurVoirMesProprietes.java
@@ -1,36 +1,57 @@
 package monopoly.vue.controleurFXML;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import monopoly.modele.Joueur;
+import monopoly.modele.cases.Case;
+import monopoly.modele.cases.Case_Achat;
+import monopoly.modele.cases.Case_Terrain;
+import monopoly.modele.cases.ECase;
 import monopoly.vue.dialogue.DialogueVoirDetailsPropriete;
 
 import java.io.IOException;
 
 public class ControleurVoirMesProprietes extends Controleur {
 
-    // TODO : Remplir le tableau
+    @FXML private TableView tableauVoirMesProprietes;
+    @FXML private TableColumn<Case_Achat, String> colNom;
+    @FXML private TableColumn<Case_Achat, String> colAmelioration;
+    @FXML private TableColumn <Case_Achat, String>colLoyer;
+    @FXML private TableColumn<Case_Achat, String> colHypotheque;
 
+    
     @FXML
-    private TableView tableauVoirMesProprietes;
+    public void initialize() {
+        colNom.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getNom()));
+        colAmelioration.setCellValueFactory(param -> {
+            Case caseParam = param.getValue();
+            SimpleStringProperty retour;
+            if(caseParam.getType() == ECase.Terrain) {
+                Case_Terrain terrain = (Case_Terrain)caseParam;
+                retour = new SimpleStringProperty(String.valueOf("$" + terrain.getPrixMaison()));
+            }
+            else {
+                retour = new SimpleStringProperty(String.valueOf("Non applicable"));
+            }
 
-    @FXML
-    private TableColumn colNom;
-
-    @FXML
-    private TableColumn colAmelioration;
-
-    @FXML
-    private TableColumn colLoyer;
-
-    @FXML
-    private TableColumn colHypotheque;
+            return retour;
+        });
+        colLoyer.setCellValueFactory(param -> new SimpleStringProperty(String.valueOf(param.getValue().getLoyer())));
+        colHypotheque.setCellValueFactory(param -> new SimpleStringProperty(String.valueOf(param.getValue().getValeurHypotheque())));
+    }
 
     @FXML
     public void actionDetailsPropriete(ActionEvent event) throws IOException {
-        new DialogueVoirDetailsPropriete();
-        // TODO : Passer une propriété en paramètre
+        Case_Terrain c = (Case_Terrain) tableauVoirMesProprietes.getSelectionModel().getSelectedItem();
+        new DialogueVoirDetailsPropriete(c);
+    }
+
+    @FXML
+    public void setJoueur(Joueur j) {
+        tableauVoirMesProprietes.getItems().addAll(j.getProprietes());
     }
 
 }
--- src/monopoly/vue/dialogue/DialogueVoirMesProprietes.java
+++ src/monopoly/vue/dialogue/DialogueVoirMesProprietes.java
@@ -1,11 +1,16 @@
 package monopoly.vue.dialogue;
 
+import monopoly.modele.Jeu;
+import monopoly.vue.controleurFXML.ControleurVoirMesProprietes;
+
 import java.io.IOException;
 
 public class DialogueVoirMesProprietes extends Dialogue {
 
     public DialogueVoirMesProprietes() throws IOException {
         super("Voir mes propriétés", "dialogueVoirMesProprietes.fxml");
+        ControleurVoirMesProprietes ctrl = (ControleurVoirMesProprietes) this.getControleur();
+        ctrl.setJoueur(Jeu.getInstance().getJoueurEnCours());
     }
 
 }
