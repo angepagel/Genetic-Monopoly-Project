2493d6d3107a560953e09e92c38405272390d609
Ange Pagel <inkandev@gmail.com>
2019-01-13 20:51:20 +0100

    Début du développement du DialogueVoirLesProprietes

--- src/monopoly/vue/controleurFXML/ControleurVoirLesProprietes.java
+++ src/monopoly/vue/controleurFXML/ControleurVoirLesProprietes.java
@@ -1,36 +1,64 @@
 package monopoly.vue.controleurFXML;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import monopoly.modele.Jeu;
+import monopoly.modele.Joueur;
+import monopoly.modele.cases.Case;
+import monopoly.modele.cases.Case_Achat;
+import monopoly.modele.cases.Case_Terrain;
+import monopoly.modele.cases.ECase;
 import monopoly.vue.dialogue.DialogueVoirDetailsPropriete;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ControleurVoirLesProprietes extends Controleur {
 
-    // TODO : Remplir le tableau
+    @FXML private TableView tableauVoirLesProprietes;
+    @FXML private TableColumn<Case_Achat, String> colNom;
+    @FXML private TableColumn<Case_Achat, String> colProprietaire;
+    @FXML private TableColumn<Case_Achat, String> colAmelioration;
+    @FXML private TableColumn<Case_Achat, String> colLoyer;
+
 
     @FXML
-    private TableView tableauVoirLesProprietes;
+    public void initialize() {
+        colNom.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getNom()));
+        colProprietaire.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getProprietaire().getNom()));
+        colAmelioration.setCellValueFactory(param -> {
+            Case caseParam = param.getValue();
+            SimpleStringProperty retour;
+            if(caseParam.getType() == ECase.Terrain) {
+                Case_Terrain terrain = (Case_Terrain)caseParam;
+                retour = new SimpleStringProperty(String.valueOf("$" + terrain.getPrixMaison()));
+            }
+            else {
+                retour = new SimpleStringProperty(String.valueOf("Non applicable"));
+            }
 
-    @FXML
-    private TableColumn colNom;
+            return retour;
+        });
+        colLoyer.setCellValueFactory(param -> new SimpleStringProperty(String.valueOf(param.getValue().getLoyer())));
 
-    @FXML
-    private TableColumn colProprietaire;
+        /*
+        ArrayList<Case> listeCases = Jeu.getInstance().getPlateau().getListeCases();
 
-    @FXML
-    private TableColumn colAmelioration;
-
-    @FXML
-    private TableColumn colLoyer;
+        for (Case c : listeCases) {
+            if (c.getType() == ECase.Terrain || c.getType() == ECase.Gare || c.getType() == ECase.Compagnie) {
+                tableauVoirLesProprietes.getItems().addAll(c);
+            }
+        }
+        */
+    }
 
     @FXML
     public void actionDetailsPropriete(ActionEvent event) throws IOException {
-        new DialogueVoirDetailsPropriete();
-        // TODO : Passer une propriété en paramètre
+        Case_Terrain c = (Case_Terrain) tableauVoirLesProprietes.getSelectionModel().getSelectedItem();
+        new DialogueVoirDetailsPropriete(c);
     }
 
 }
--- src/monopoly/vue/dialogue/DialogueVoirLesProprietes.java
+++ src/monopoly/vue/dialogue/DialogueVoirLesProprietes.java
@@ -1,12 +1,8 @@
 package monopoly.vue.dialogue;
 
-import monopoly.modele.cases.Case_Achat;
-
 import java.io.IOException;
 
 public class DialogueVoirLesProprietes extends Dialogue {
-
-    private Case_Achat caseAchat;
 
     public DialogueVoirLesProprietes() throws IOException {
         super("Voir les propriétés", "DialogueVoirLesProprietes.fxml");
