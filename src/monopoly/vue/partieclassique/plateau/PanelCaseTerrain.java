fd94d06004aa3eeabc11e54ff9c83d62c807b9a7
Sekanor <sekanor21@gmail.com>
2019-01-13 17:25:37 +0100

    Ajout d'un dialogue avec les informations d'un terrain lorsqu'on clique dessus
    + mais ne fonctionne pas pour toutes les cases, souci de collision

--- src/monopoly/vue/partieclassique/plateau/PanelCaseTerrain.java
+++ src/monopoly/vue/partieclassique/plateau/PanelCaseTerrain.java
@@ -1,8 +1,10 @@
 package monopoly.vue.partieclassique.plateau;
 
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
@@ -12,6 +14,9 @@ import javafx.scene.text.TextAlignment;
 import monopoly.modele.Jeu;
 import monopoly.modele.cases.Case;
 import monopoly.modele.cases.Case_Terrain;
+import monopoly.vue.dialogue.DialogueVoirDetailsPropriete;
+
+import java.io.IOException;
 
 /**
  * Created by maxim on 23/11/2018.
@@ -28,6 +33,8 @@ public class PanelCaseTerrain extends PanelCase {
     public PanelCaseTerrain(String nom) {
         super(nom);
 
+        PanelCaseTerrain self = this;
+
         this.modele = (Case_Terrain) this.getModele();
 
         // Component properties
@@ -38,6 +45,18 @@ public class PanelCaseTerrain extends PanelCase {
         this.prix.setPadding(new Insets(0,0,0,0));
 
         this.getChildren().addAll(this.header, this.nom, this.emplacements, this.prix);
+
+        this.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                try {
+                    new DialogueVoirDetailsPropriete(self.modele);
+                }
+                catch (IOException e) {
+
+                }
+            }
+        });
 
     }
 
